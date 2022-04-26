c27bfe207f850cd9101c42ac7f6c749649230a69
esquivelcarlosd <74650214+esquivelcarlosd@users.noreply.github.com>
2022-04-25 23:04:49 -0500

    Create  loEsencialDelCicloWhileAdivinaElNUmeroSecreto.py

---
+++ Módulo 3 Valores booleanos, ejecución condicional, bucles, listas y procesamiento de listas, operaciones lógicas y bit a bitExternal tool/loEsencialDelCicloWhileAdivinaElNUmeroSecreto.py
@@ -0,0 +1,45 @@
+numeroSecreto = 777
+
+
+print(
+"""
++==================================+
+| Bienvenido a mi juego, muggle!   |
+| Introduce un número entero       |
+| y adivina qué número he          |
+| elegido para ti.                 |
+| Entonces,                        |
+| ¿Cuál es el número secreto?      |
++==================================+
+""")
+
+
+ingNum = int(input("Ingresa un número entero: "))
+
+while ingNum != numeroSecreto:
+    print("¡Ja, ja! ¡Estás atrapado en mi ciclo!")
+    ingNum = int(input("Ingresa un número entero: "))
+
+print("¡Bien hecho, muggle! Eres libre ahora")
+
+"""•	3.1.2.3 LABORATORIO: Lo esencial del ciclo while - Adivina el número secreto
+•	
+LABORATORIO
+
+
+Tiempo estimado
+15 minutos
+Nivel de dificultad
+Fácil
+Objetivos
+Familiarizar al estudiante con:
+•	Utilizar el ciclo while.
+•	Reflejar situaciones de la vida real en código de computadora.
+Escenario
+Un mago junior ha elegido un número secreto. Lo ha escondido en una variable llamada númeroSecreto. Quiere que todos los que ejecutan su programa jueguen el juego Adivina el número secreto, y adivina qué número ha elegido para ellos. ¡Quienes no adivinen el número quedarán atrapados en un ciclo sin fin para siempre! Desafortunadamente, él no sabe cómo completar el código.
+Tu tarea es ayudar al mago a completar el código en el editor de tal manera que el código:
+•	Pedirá al usuario que ingrese un número entero.
+•	Utilizará un ciclo while.
+•	Comprobará si el número ingresado por el usuario es el mismo que el número escogido por el mago. Si el número elegido por el usuario es diferente al número secreto del mago, el usuario debería ver el mensaje "¡Ja, ja! ¡Estás atrapado en mi ciclo!"  y se le solicitará que ingrese un número nuevamente. Si el número ingresado por el usuario coincide con el número escogido por el mago, el número debe imprimirse en la pantalla, y el mago debe decir las siguientes palabras: "¡Bien hecho, muggle! Eres libre ahora".
+¡El mago está contando contigo! No lo decepciones.
+"""
